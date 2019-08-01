8560ee4b9ee05abafba2db50fd2b0ee5dcc213c2
yushiguang <yushiguang@pinduoduo.com>
2019-08-01 15:39:50 +0800

    提交基本的语法

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,24 @@
-#include<iostream>
-#include<limits>
+#include <iostream>
+#include <limits>
 using namespace std;
-int main(){
-   cout <<"Hello ,world"<< endl;
+typedef int inttype;
+int main()
+{
+    // cout 输出语句
+    cout << "Hello ,world" << endl;
+    //测试基本数据结构所占空间大小
+    cout << "bool: \t\t"
+         << "所占的字节数： " << sizeof(int) << endl;
+    cout << "\t 最大值" << (numeric_limits<int>::max)();
 
-   cout << "bool: \t\t" << "所占的字节数： " << sizeof(int)<<endl;
-   cout << "\t 最大值" << (numeric_limits<int>::max)();
-   return 0;
+    // typedef 改变相关类型昵称
+    inttype a = 3;
+
+    //枚举类型(默认情况下第一个名称值为0，第二个值为1)
+    enum color {red,green,blue} c;
+    c = blue; // 此时blue = 2;
+
+    //定义变量
+    int i,j;
+    return 0;
 }
